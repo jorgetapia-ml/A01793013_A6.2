608c1a03669612841ac91469d932126da349c899
jorge tapia <jorge@gmail.com>
2024-02-16 23:30:49 -0600

    folder added

--- src/hotel_unitest.py
+++ src/hotel_unitest.py
@@ -1,7 +1,7 @@
 """unittest module"""
 import unittest
 import os
-from .__init__ import Hotel, Customer, Reservation
+from .hostelry_module import Hotel, Customer, Reservation
 
 class TestHotelManagementSystem(unittest.TestCase):
     """
