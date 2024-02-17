5df802c79d66c15f9eb022c73373306c00691520
jorge tapia <jorge@gmail.com>
2024-02-17 01:15:50 -0600

    bash file added

---
+++ src/make_test.sh
@@ -0,0 +1,21 @@
+log_file="hostelry.txt"
+paths=(
+    "hostelry_module/customer.py"
+    "hostelry_module/reservation.py"
+    "hostelry_module/hotel.py"
+)
+
+for i in "${!paths[@]}"; do
+    path="${paths[$i]}"
+    echo "-------------------Testing with $path-------------------"
+    
+    echo "----------------PyLint-------------------" | tee -a "$log_file"
+    pylint $path | tee -a "$log_file"
+
+    echo "----------------flake8-------------------" | tee -a "$log_file"
+    flake8 $path | tee -a "$log_file"
+    echo "flake8 finish" | tee -a "$log_file"
+done
+
+echo "----------------Coverage-------------------" | tee -a "$log_file"
+coverage run -m unittest hotel_unitest.py | tee -a "$log_file"
